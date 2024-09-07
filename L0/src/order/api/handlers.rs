17e5269692bc5ed9a8204b2bf9ef147506cb9782
ulurn-av <ulurn@yandex.ru>
2024-09-07 09:14:52 +0700

    init models

--- L0/src/order/api/handlers.rs
+++ L0/src/order/api/handlers.rs
@@ -1,6 +1,11 @@
-pub async fn get_order_by_uid() {
+use axum::extract::{Path, Json};
+use crate::order::models::Order;
+
+pub async fn get_order_by_uid(Path(uid): Path<String>) {
+
 }
 
-pub async fn create_order() {
+pub async fn create_order(Json(payload) : Json<Order>) {
+
 }
 
---
+++ L0/src/order/models.rs
@@ -0,0 +1,60 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Delivery {
+    name: String,
+    phone: String,
+    zip: String,
+    city: String,
+    address: String,
+    region: String,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Payment {
+    transaction: String,
+    request_id: String,
+    currency: String,
+    provider: String,
+    amount: u32,
+    payment_dt: u64,
+    bank: String,
+    delivery_cost: u32,
+    goods_total: u32,
+    custom_fee: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    chrt_id: u32,
+    track_number: String,
+    price: u32,
+    rid: String,
+    name: String,
+    sale: u32,
+    size: String,
+    total_price: u32,
+    nm_id: u32,
+    brand: String,
+    status: u32,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Order {
+    order_uid: String,
+    track_number: String,
+    entry: String,
+    delivery: Delivery,
+    payment: Payment,
+    items: Vec<Item>,
+    locale: String,
+    internal_signature: String,
+    customer_id: String,
+    delivery_service: String,
+    shardkey: String,
+    sm_id: u32,
+    date_created: String,
+    oof_shard: String,
+}
